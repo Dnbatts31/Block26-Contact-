71a1b298f77f7fdf875a90951ed8da4a0fbaa762
Demeka Batts <demekabatts@Natalies-MacBook-Air.local>
2024-04-13 20:52:55 -0400

    complete

---
+++ Block26/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import ContactDetails from './components/ContactDetails';
+import ContactList from './components/ContactList'
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+  const [detailedContact, setDetailedContact] = useState(null);
+
+  useEffect(() => {
+    async function fetchSelectedContact() {
+      try {
+        const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/" + selectedContactId);
+        const result = await response.json();
+        setDetailedContact(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    if (selectedContactId) {
+      fetchSelectedContact()
+    }
+  }, [selectedContactId])
+
+
+  return (
+    <>
+      {detailedContact ? (<ContactDetails detailedContact={detailedContact}/>) : (<ContactList setSelectedContactId={setSelectedContactId} />)}
+    </>
+  )
+}
+
+export default App
